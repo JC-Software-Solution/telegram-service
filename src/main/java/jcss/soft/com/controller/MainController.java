cd9a7ea360f8507027cc66ae1edc3a4b87d0bcd5
Jovanie Cabatuan <jhovaniecabatuan@gmail.com>
2026-06-29 17:23:08 +0800

    added controller

---
+++ src/main/java/jcss/soft/com/controller/MainController.java
@@ -0,0 +1,16 @@
+package jcss.soft.com.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bot")
+public class MainController {
+
+    @GetMapping
+    public ResponseEntity<String> getName() {
+        return ResponseEntity.ok("Hello");
+    }
+}
